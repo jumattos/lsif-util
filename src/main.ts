a5a5277dc90e0e9cbd70ff1e5929895a2aa720bb
Jullyana Mattos Vycas <jumattos@microsoft.com>
2019-05-28 12:09:30 -0700

    Enable stdin option

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
 import * as fse from 'fs-extra';
 import * as LSIF from 'lsif-protocol';
+import * as readline from 'readline';
 import * as yargs from 'yargs';
 import { getFilteredIds, IFilter } from './filter';
 import { validate } from './validate';
 import { visualize } from './visualize';
 
-function getInput(format: any, path: string): LSIF.Element[] {
-    // TODO: support stdin (using the stdin flag or special file name `-`)
-    switch (format) {
-        case 'json':
-            return fse.readJSONSync(path);
-        case 'line': case undefined:
-            return fse.readFileSync(path, 'utf8').split("\r\n").map(line => JSON.parse(line));
+function readInput(format: any, path: string, callback: (input: LSIF.Element[]) => void) {
+    let inputStream: NodeJS.ReadStream | fse.ReadStream = process.stdin;
+    if (path) {
+        inputStream = fse.createReadStream(path);
     }
+
+    let input: LSIF.Element[] = [];
+    let buffer: string[] = [];
+    const rd = readline.createInterface(inputStream);
+    rd.on('line', line => {
+        switch (format) {
+            case 'json':
+                buffer.push(line);
+            case 'line': default:
+                input.push(JSON.parse(line));
+        }
+    });
+
+    rd.on('close', () => {
+        if (buffer.length > 0) {
+            input = JSON.parse(buffer.join());
+        }
+
+        callback(input);
+    });
 }
 
 async function main() {
@@ -24,9 +42,10 @@ async function main() {
             default: './lsif.json'
         })
     }, argv => {
-        const input = getInput(argv.inputFormat, argv.file);
-        const filter = <IFilter> <unknown>argv;
-        validate(input, getFilteredIds(filter, input));
+        readInput(argv.inputFormat, argv.stdin ? undefined : argv.file, input => {
+            const filter = <IFilter> <unknown>argv;
+            validate(input, getFilteredIds(filter, input));
+        });
     })
     .command('visualize [file]', '', yargs => {
         return yargs
@@ -39,9 +58,10 @@ async function main() {
             default: 1
         })
     }, argv => {
-        const input = getInput(argv.inputFormat, argv.file);
-        const filter = <IFilter> <unknown>argv;
-        visualize(input, getFilteredIds(filter, input), argv.distance);
+        readInput(argv.inputFormat, argv.stdin ? undefined : argv.file, input => {
+            const filter = <IFilter> <unknown>argv;
+            visualize(input, getFilteredIds(filter, input), argv.distance);
+        });
     })
     .demandCommand(1, 1) // One and only one command should be specified
     .option('inputFormat', { default: 'line', choices: ['line', 'json'], description: 'Specify input format' })
